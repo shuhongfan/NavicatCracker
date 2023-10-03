b6d48a57d857c649274f1f8c6f69ca1f5968a036
舒洪凡ShuHongFan <shfshuhongfan@gmail.com>
2023-10-03 20:42:31 +0800

    V16.2

---
+++ V16.2-无限试用/无限试用Navicat.bat
@@ -0,0 +1,15 @@
+@echo off
+ 
+echo Delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration[version and language]
+for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium" /s | findstr /L Registration"') do (
+    reg delete %%i /va /f
+)
+echo.
+ 
+echo Delete Info folder under HKEY_CURRENT_USER\Software\Classes\CLSID
+for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s | findstr /E Info"') do (
+    reg delete %%i /va /f
+)
+echo.
+ 
+echo Finish
